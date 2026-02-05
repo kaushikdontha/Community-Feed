26675bf81957aff42387763c6e34058b43496ca4
kaushik <donthakaushik@gmail.com>
2026-02-05 11:39:38 +0530

    Add Vercel deployment configuration

--- backend/community_feed/wsgi.py
+++ backend/community_feed/wsgi.py
@@ -9,3 +9,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'community_feed.settings')
 
 application = get_wsgi_application()
+
+# Vercel serverless expects 'app' variable
+app = application
